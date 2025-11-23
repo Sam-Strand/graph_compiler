4c866a9a1618138db92d94fad5d8ba0bfd515c0b
Maxim <i@maxim-sadovskiy.ru>
2025-11-23 18:06:38 +0300

    Обработка нод переменных

--- graph_compiler/graph.py
+++ graph_compiler/graph.py
@@ -24,8 +24,63 @@ def find_reachable_nodes(start_nodes: Set[str], reverse_graph: Dict[str, List[st
     return visited
 
 
+def process_variable_nodes(json_data: Dict[str, Any]) -> Dict[str, Any]:
+    '''Обрабатывает variable ноды, заменяя их реальными соединениями'''
+    variable_nodes = [node for node in json_data['nodes'] if node.get('type') == 'variable']
+    
+    variable_groups = defaultdict(list)
+    for node in variable_nodes:
+        label = node.get('data', {}).get('label', '')
+        if label:
+            variable_groups[label].append(node)
+    
+    connections_by_target = defaultdict(list)
+    connections_by_source = defaultdict(list)
+    
+    for conn in json_data['connections']:
+        connections_by_target[conn['target']].append(conn)
+        connections_by_source[conn['source']].append(conn)
+    
+    new_connections = []
+    connections_to_remove = []
+    
+    for label, nodes in variable_groups.items():
+        input_node = next((node for node in nodes if node.get('data', {}).get('is_input')), None)
+        output_nodes = [node for node in nodes if not node.get('data', {}).get('is_input')]
+        
+        if not input_node or not output_nodes:
+            continue
+        
+        input_connections = connections_by_target.get(input_node['id'], [])
+        
+        for output_node in output_nodes:
+            output_connections = connections_by_source.get(output_node['id'], [])
+            
+            for input_conn in input_connections:
+                for output_conn in output_connections:
+                    new_connections.append({
+                        'source': input_conn['source'],
+                        'target': output_conn['target'],
+                        'targetInput': output_conn['targetInput']
+                    })
+            
+            connections_to_remove.extend(input_connections)
+            connections_to_remove.extend(output_connections)
+    
+    json_data['connections'] = [
+        conn for conn in json_data['connections'] 
+        if conn not in connections_to_remove
+    ]
+    
+    json_data['connections'].extend(new_connections)
+    
+    return json_data
+
+
 def optimize_graph(json_data: Dict[str, Any]) -> Dict[str, Any]:
     '''Оптимизирует граф, удаляя неиспользуемые узлы'''
+    json_data = process_variable_nodes(json_data)
+    
     output_nodes = {
         node['id'] for node in json_data['nodes']
         if node.get('type') == 'out'
